6fea82ced7b7109f38ee61c3606f36dd36f55039
Suvam Khanal <khanalsuvam28@gmail.com>
2025-11-27 14:49:19 +0545

    getApi changes

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
 // alternative way 
-const{connectDatabase} = require("./database/database");
+const { connectDatabase } = require("./database/database");
 const Blog = require("./Model/blogModel")
 const express = require("express")
 const app = express()
 connectDatabase();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-    console.log("Mangodb sanga connect vayo hai!");
+console.log("Mangodb sanga connect vayo hai!");
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.json({
-        message:"yo home page hai",
+        message: "yo home page hai",
     })
 })
 
-app.get("/contact",(haha,huhu)=>{
+app.get("/contact", (haha, huhu) => {
     huhu.send("This is Contact Page Hai");
 })
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello I am From Home Page")
 })
 
-app.listen(3000,(req,res)=>{
+app.listen(3000, (req, res) => {
     console.log("Node js started at port 3000")
 });
 
 
 //CREATE BLOG API
 
-app.post("/createBlog",async(req,res)=>{
+app.post("/createBlog", async (req, res) => {
 
     // const title = req.body.title;
     // const subtitle = req.body.subtitle;
     // const description = req.body.description;
 
-//Alternative (object destructuring)
-const {title,subtitle,description} = req.body
-console.log(req.body)
+    //Alternative (object destructuring)
+    const { title, subtitle, description } = req.body
+    console.log(req.body)
 
 
-// Insert to database logic goes here
+    // Insert to database logic goes here
 
-await Blog.create({
-    title,
-    subtitle,
-    description
-});
+    await Blog.create({
+        title,
+        subtitle,
+        description
+    });
 
 
 
@@ -57,8 +57,49 @@ await Blog.create({
         status: 201,
         message: "Blog created successfully",
     });
-});
 
+});
+app.get("/blogs", async (req, res) => {
+    //fetching/reading all blogs from Blog Model
+    const blogs = await Blog.find();
+
+    if (blogs.length == 0) {
+        req.json({
+            status: 404,
+            message: "Empty Blog",
+        });
+    } else {
+        res.json({
+            status: 200,
+            message: "Blogs Fetched Successfully",
+            data: blogs, 
+        });
+    }
+}
+)
+
+//Get => get single blog
+app.get("/blogs/:id", async(req,res)=>{
+    console.log(req.params.id);
+    const id = req.params.id;
+    //const (id) req.params Alternative
+    // const blog = await Blog.find({_id: id});
+    //Alternative
+    const blog = await Blog.findById(id)
+
+    //if(blog.length == 0){
+    // res.status(404).json({
+    //message:"No Blog found with that id"
+    //})
+    //}else{
+    
+        res.status(200).json({
+            message:"Blogs Fetched Successfully",
+            data: blog,
+        })
+    //}
+}
+)
 
 
 
